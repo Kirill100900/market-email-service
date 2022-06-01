3f507e131fb63617f94b9f6395e7d303480ea4d9
Kosimov Rustam <rustma.ru26@mail.ru>
2022-06-01 17:30:02 +0300

    Тестовый реактивный контроллер на отправку письма

--- src/main/java/market/controller/EmailController.java
+++ src/main/java/market/controller/EmailController.java
@@ -18,9 +18,10 @@ public class EmailController {
         this.mailService = mailService;
     }
 
-    @GetMapping(value = "/email/{user-email}")
-    public Mono<ResponseEntity> sendEmail(@PathVariable("user-email") String email) {
-        MailMessageDto mailMessageDto = new MailMessageDto(email, "", "", MailLanguages.RU);
+    @GetMapping(value = "/email/{user-email/{username}")
+    public Mono<ResponseEntity> sendEmail(@PathVariable("user-email") String email,
+                                          @PathVariable("username") String username) {
+        MailMessageDto mailMessageDto = new MailMessageDto(email, username, "", MailLanguages.RU);
         return Mono.fromCallable(() -> {
                     try {
                         mailService.sendMessageUsingFreemarkerTemplate(mailMessageDto);
